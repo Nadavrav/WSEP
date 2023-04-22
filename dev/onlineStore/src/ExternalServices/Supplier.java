0193ee29062724a24d32eff26c6598d8a71ccb96
Nadavrav <81586883+Nadavrav@users.noreply.github.com>
2023-04-22 16:46:11 +0300

    Update Supplier.java

--- dev/onlineStore/src/ExternalServices/Supplier.java
+++ dev/onlineStore/src/ExternalServices/Supplier.java
@@ -1,4 +1,17 @@
 package ExternalServices;
 
 public class Supplier {
+  
+  public Supplier(){
+  }
+  
+  public boolean isValidAddress(String address){
+    return true;
+  }
+  
+  public boolean supplyProducts(List<String> products){
+    return true;
+  }
+  
+  
 }
