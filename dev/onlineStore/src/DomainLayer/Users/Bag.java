59ec98a7a175730aeca41b0653bd86a2a49db0f9
Nadavrav <81586883+Nadavrav@users.noreply.github.com>
2023-04-22 16:41:06 +0300

    Added CaculateTotalAmount and GetStoreID functions

--- dev/onlineStore/src/DomainLayer/Users/Bag.java
+++ dev/onlineStore/src/DomainLayer/Users/Bag.java
@@ -14,7 +14,26 @@ public class Bag {
     public void addProduct(StoreProduct product) {
         productList.put(product.getId(),product);
     }
-
+    
+    public int calculateTotalAmount(){
+        int totalAmount = 0;
+        foreach(StoreProduct sp : productList.Values){
+            totalAmount += sp.getPrice();
+        }
+        return totalAmount;
+    }
+    
+    public String getStoreID(){
+        if(productList.isEmpty()){
+            return "";
+        }
+        else{
+            String productId = productList.get(0);
+            String storeId = productId.substring(0,productId.indexOf('-'));
+            return storeId;
+        }
+    }
+    
     public String bagToString() {
         String s="";
         for (String i :productList.keySet()) {
