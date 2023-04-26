95f9da86df0115d337ac931e360ab01d8ff17016
majd12-ux <80644145+majd12-ux@users.noreply.github.com>
2023-04-26 23:29:38 +0300

    update logger

--- dev/onlineStore/src/DomainLayer/Users/Employment.java
+++ dev/onlineStore/src/DomainLayer/Users/Employment.java
@@ -19,13 +19,12 @@ public class Employment {
 
 
      public Employment (RegisteredUser appointer, RegisteredUser employee, Store store, Role role){
-        try {
-            Handler handler = new FileHandler("Info.txt");
-            Handler handler1 = new FileHandler("Error.txt");
-            logger.addHandler(handler);
-            logger.addHandler(handler1);
-            handler.setFormatter(new SimpleFormatter());
-            handler1.setFormatter(new SimpleFormatter());
+       try{
+            UniversalHandler.GetInstance().HandleError(logger);
+            UniversalHandler.GetInstance().HandleInfo(logger);
+        }
+        catch (Exception ignored){
+        }
             this.appointer=appointer;
             this.employee=employee;
             this.store=store;
@@ -39,28 +38,21 @@ public class Employment {
                 permissions.add(Permission.CanSeeCommentsAndRating);
                 permissions.add(Permission.CanSeePurchaseHistory);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
     }
 
     public Employment ( RegisteredUser employee, Store store, Role role){
-        try {
-            Handler handler = new FileHandler("Info.txt");
-            Handler handler1 = new FileHandler("Error.txt");
-            logger.addHandler(handler);
-            logger.addHandler(handler1);
-            handler.setFormatter(new SimpleFormatter());
-            handler1.setFormatter(new SimpleFormatter());
+        try{
+            UniversalHandler.GetInstance().HandleError(logger);
+            UniversalHandler.GetInstance().HandleInfo(logger);
+        }
+        catch (Exception ignored){
+        }
             this.appointer = null;
             this.employee = employee;
             this.store = store;
             this.role = role;
             permissions = null;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        
 
     }
 
