bc5b14f9a624a5318367527cbdaca953db3609aa
majd12-ux <80644145+majd12-ux@users.noreply.github.com>
2023-04-26 23:15:35 +0300

    logger update
    logger is done here

--- dev/onlineStore/src/DomainLayer/Users/Employment.java
+++ dev/onlineStore/src/DomainLayer/Users/Employment.java
@@ -3,6 +3,10 @@ package DomainLayer.Users;
 import DomainLayer.Stores.Store;
 
 import java.util.LinkedList;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Employment {
     private RegisteredUser appointer;
@@ -10,31 +14,53 @@ public class Employment {
     private Store store;
     private Role role;
     private LinkedList<Permission> permissions;
+    private static final Logger logger=Logger.getLogger("Employment logger");
 
 
 
-    public Employment (RegisteredUser appointer, RegisteredUser employee, Store store, Role role){
-        this.appointer=appointer;
-        this.employee=employee;
-        this.store=store;
-        this.role=role;
-        permissions = new LinkedList<>();
+     public Employment (RegisteredUser appointer, RegisteredUser employee, Store store, Role role){
+        try {
+            Handler handler = new FileHandler("Info.txt");
+            Handler handler1 = new FileHandler("Error.txt");
+            logger.addHandler(handler);
+            logger.addHandler(handler1);
+            handler.setFormatter(new SimpleFormatter());
+            handler1.setFormatter(new SimpleFormatter());
+            this.appointer=appointer;
+            this.employee=employee;
+            this.store=store;
+            this.role=role;
+            permissions = new LinkedList<>();
 
-        if(role == Role.StoreOwner){
+            if(role == Role.StoreOwner){
 
+            }
+            else if(role == Role.StoreManager){
+                permissions.add(Permission.CanSeeCommentsAndRating);
+                permissions.add(Permission.CanSeePurchaseHistory);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        else if(role == Role.StoreManager){
-            permissions.add(Permission.CanSeeCommentsAndRating);
-            permissions.add(Permission.CanSeePurchaseHistory);
-        }
+
     }
 
     public Employment ( RegisteredUser employee, Store store, Role role){
-        this.appointer=null;
-        this.employee=employee;
-        this.store=store;
-        this.role=role;
-        permissions = null;
+        try {
+            Handler handler = new FileHandler("Info.txt");
+            Handler handler1 = new FileHandler("Error.txt");
+            logger.addHandler(handler);
+            logger.addHandler(handler1);
+            handler.setFormatter(new SimpleFormatter());
+            handler1.setFormatter(new SimpleFormatter());
+            this.appointer = null;
+            this.employee = employee;
+            this.store = store;
+            this.role = role;
+            permissions = null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -65,15 +91,23 @@ public class Employment {
     }
 
     public void togglePermission(Permission p){
-        if(permissions.contains(p)){
+        if (permissions.contains(p)) {
             permissions.remove(p);
-        }
-        else{
+            logger.info("Permission " + p.toString() + " removed.");
+        } else {
             permissions.add(p);
+            logger.info("Permission " + p.toString() + " added.");
         }
-}
-    public boolean checkIfStoreManager() {
-        return (getRole()== Role.StoreFounder ||  getRole()==Role.StoreManager);
+    }
+    
+     public boolean checkIfStoreManager() {
+        boolean isStoreManager = role == Role.StoreFounder || role == Role.StoreManager;
+        if (isStoreManager) {
+            logger.info("User is a store manager.");
+        } else {
+            logger.info("User is not a store manager.");
+        }
+        return isStoreManager;
 
     }
     @Override
