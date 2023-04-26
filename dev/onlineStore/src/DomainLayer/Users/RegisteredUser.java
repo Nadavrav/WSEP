1981c93b707bef335a743dadef55317b20fb432f
majd12-ux <80644145+majd12-ux@users.noreply.github.com>
2023-04-26 23:12:47 +0300

    logger update
    logger is donr here

--- dev/onlineStore/src/DomainLayer/Users/RegisteredUser.java
+++ dev/onlineStore/src/DomainLayer/Users/RegisteredUser.java
@@ -16,32 +16,57 @@ public class RegisteredUser extends SiteVisitor{
     public RegisteredUser(String userName, String password) throws Exception {
         super(0);
 
-        checkUserName(userName);
-        checkPassword(password);
-        this.userName=userName;
-        this.password=password;
-        this.purchaseHistory = new PurchaseHistory();
+        try {
+            Handler handler = new FileHandler("Info.txt");
+            Handler handler1 = new FileHandler("Error.txt");
+            logger.addHandler(handler);
+            logger.addHandler(handler1);
+            handler.setFormatter(new SimpleFormatter());
+            handler1.setFormatter(new SimpleFormatter());
+            checkUserName(userName);
+            checkPassword(password);
+            this.userName=userName;
+            this.password=password;
+            this.purchaseHistory = new PurchaseHistory();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public RegisteredUser(SiteVisitor visitor,String userName, String password) {
+     public RegisteredUser(SiteVisitor visitor,String userName, String password) {
         super(visitor);
-
-        checkUserName(userName);
-        checkPassword(password);
-        this.userName=userName;
-        this.password=password;
-        this.purchaseHistory = new PurchaseHistory();
+        try {
+            Handler handler = new FileHandler("Info.txt");
+            Handler handler1 = new FileHandler("Error.txt");
+            logger.addHandler(handler);
+            logger.addHandler(handler1);
+            handler.setFormatter(new SimpleFormatter());
+            handler1.setFormatter(new SimpleFormatter());
+            checkUserName(userName);
+            checkPassword(password);
+            this.userName=userName;
+            this.password=password;
+            this.purchaseHistory = new PurchaseHistory();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 
     private void checkPassword(String password) {
         if(password==null) {
+            logger.warning("null password");
             throw new IllegalArgumentException("Username cannot be null");
         }
         if(password.length()<8){
+            logger.warning("invalid password");
             throw new IllegalArgumentException("the password is too short");
         }
         if(password.length()>30){
+            logger.warning("invalid password");
             throw new IllegalArgumentException("the password is too long");
         }
     }
@@ -49,12 +74,15 @@ public class RegisteredUser extends SiteVisitor{
     private void checkUserName(String userName) {
 
         if (userName == null) {
+            logger.warning("null username");
             throw new IllegalArgumentException("Username cannot be null");
         }
         if (userName.length() < 8) {
+            logger.warning("invalid username");
             throw new IllegalArgumentException("the userName is too short");
         }
         if (userName.length() > 30) {
+            logger.warning("invalid username");
             throw new IllegalArgumentException("the useName is too long");
         }
     }
@@ -67,13 +95,24 @@ public class RegisteredUser extends SiteVisitor{
     }
 
     public void login(String password, int visitorId) throws Exception {//1.4
-        if (!this.password.equals(password)){
-            throw  new IllegalArgumentException("wrong password");
+        logger.info("Attempting login for visitor with ID: " + visitorId);
+
+        // Check password
+        if (!this.password.equals(password)) {
+            // Log login failure
+            logger.warning("Failed login attempt for visitor with ID: " + visitorId);
+            throw new IllegalArgumentException("Wrong password");
         }
-        if( getVisitorId()!=0){//visitorId =0 mean the user is logged out
-            throw  new Exception("this user is already login");
+        // Check if user is already logged in
+        if (getVisitorId() != 0) {
+            // Log login failure
+            logger.warning("Failed login attempt for visitor with ID: " + visitorId + ". User is already logged in.");
+            throw new Exception("This user is already logged in");
         }
+        // Set visitor ID
         setVisitorId(visitorId);
+        // Log successful login
+        logger.info("Successfully logged in visitor with ID: " + visitorId);
     }
     public void logout(){//3.1
         setVisitorId(0);
