a25d424b6aabd7a5c2df21350027976be28d35a3
DenisPn <panor@post.bgu.co.il>
2023-06-02 15:14:52 +0300

    MAYBE fixed the maven imploding again, tests are running, and I've possibly fixed the compiled (.class) files getting in the way with git

--- dev/onlineStore/src/test/UnitTests/BagTest.java
+++ dev/onlineStore/src/test/UnitTests/BagTest.java
@@ -4,7 +4,6 @@ import DomainLayer.Stores.Products.StoreProduct;
 import DomainLayer.Users.Bag;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
