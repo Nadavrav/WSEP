d09b95369a478b5cdecd6e7876cfba364b157ce0
DenisPn <panor@post.bgu.co.il>
2023-06-03 01:12:04 +0300

    changed ServiceDiscount to be a record

--- dev/onlineStore/src/main/java/ServiceLayer/ServiceObjects/ServiceDiscounts/ServiceDiscount.java
+++ dev/onlineStore/src/main/java/ServiceLayer/ServiceObjects/ServiceDiscounts/ServiceDiscount.java
@@ -1,20 +1,5 @@
 package ServiceLayer.ServiceObjects.ServiceDiscounts;
 
 
-import DomainLayer.Stores.Conditions.ConditionTypes.Condition;
-
-import java.util.Set;
-
-public class ServiceDiscount {
-    private final String discountDescription;
-    private final int id;
-
-    public ServiceDiscount(String discountDescription, int id) {
-        this.discountDescription = discountDescription;
-        this.id=id;
-    }
-
-    public String getDiscountDescription() {
-        return discountDescription;
-    }
+public record ServiceDiscount(String discountDescription, int id) {
 }
